8abb1c095386fce4e11f2c42fc0432d99aa1eef9
Anwesh43 <anweshthecool0@gmail.com>
2019-04-08 19:15:58 +0530

    adding constants and libraries

--- semicircleteethview/src/main/java/com/anwesh/uiprojects/semicircleteethview/SemiCircleTeethView.kt
+++ semicircleteethview/src/main/java/com/anwesh/uiprojects/semicircleteethview/SemiCircleTeethView.kt
@@ -4,3 +4,22 @@ package com.anwesh.uiprojects.semicircleteethview
  * Created by anweshmishra on 08/04/19.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Color
+import android.content.Context
+import android.app.Activity
+
+val nodes : Int = 5
+val squares : Int = 4
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val rotDeg : Float = 90f
+val foreColor : Int = Color.parseColor("#673AB7")
+val backColor : Int = Color.parseColor("#BDBDBD")
+
