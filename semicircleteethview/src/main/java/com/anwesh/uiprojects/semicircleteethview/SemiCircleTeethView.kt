6f572cf02bbf2e73ba53df4e4d5d139259f5566e
Anwesh43 <anweshthecool0@gmail.com>
2019-04-08 20:23:48 +0530

    adding delay as constant

--- semicircleteethview/src/main/java/com/anwesh/uiprojects/semicircleteethview/SemiCircleTeethView.kt
+++ semicircleteethview/src/main/java/com/anwesh/uiprojects/semicircleteethview/SemiCircleTeethView.kt
@@ -23,6 +23,7 @@ val rotDeg : Float = 180f
 val foreColor : Int = Color.parseColor("#673AB7")
 val backColor : Int = Color.parseColor("#BDBDBD")
 val parts : Int = 2
+val delay : Long = 20
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
@@ -111,7 +112,7 @@ class SemiCircleTeethView(ctx : Context) : View(ctx) {
             if (animated) {
                 cb()
                 try {
-                    Thread.sleep(50)
+                    Thread.sleep(delay)
                     view.invalidate()
                 } catch(ex : Exception) {
 
