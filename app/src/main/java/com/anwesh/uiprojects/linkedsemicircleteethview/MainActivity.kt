256dc3a525682bc712459970f716ca47e331c8e8
Anwesh43 <anweshthecool0@gmail.com>
2019-04-08 20:10:23 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedsemicircleteethview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedsemicircleteethview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedsemicircleteethview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.semicircleteethview.SemiCircleTeethView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        SemiCircleTeethView.create(this)
     }
 }
